fcdda8eff832856f414f57c794f664e5aa35cf50
Gaurav <gaurav556pant@gmail.com>
2023-07-04 18:03:24 +0530

    Updating on A Navbar and Footer

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,13 @@
-$(document).ready
+$(document).ready(function () {
+
+    // $('.navbar-toggle').click(function () {
+    //     $(this).toggleClass('active');
+    //     $('.navbar-menu').slideToggle('fast');
+    // });
+
+    // $('.dropbtn').click(function (e) {
+    //     e.preventDefault();
+    //     $(this).parent().toggleClass('active');
+    //     $(this).next('.dropdown-content').slideToggle('fast');
+    // });
+});
