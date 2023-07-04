b87f9c7f1e9abe55136f925bb0c1c8ef530a3188
Gaurav <gaurav556pant@gmail.com>
2023-07-04 21:58:50 +0530

    Adding a login and signup

---
+++ src/main/java/net/resume/building/controller/RegistrationController.java
@@ -0,0 +1,13 @@
+package net.resume.building.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegistrationController {
+    
+	@GetMapping("/registra")
+	public String registraPage() {
+		return "registra";
+	}
+}
